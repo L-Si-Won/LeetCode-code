c8205ce23e06e89bbeb2f64af2565607b0b91dfc
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-12-28 15:12:51 +0900

    Create 607. Sales Person.sql

---
+++ SQL/EASY/607. Sales Person.sql
@@ -0,0 +1,22 @@
+select name
+from SalesPerson
+where sales_id not in(
+    select sales_id
+    from Orders left join Company
+    On Orders.com_id=Company.com_id
+    where Company.name='RED'
+)
+
+======================================================
+
+select name
+from SalesPerson
+where sales_id not in(
+    select sales_id
+    from Orders 
+    where com_id in(
+        select com_id
+        from Company
+        where name='RED'
+    )
+)
